16d029b731e33f33833e2d6b5a07349c7c4ef788
Jack <me@jackmerrill.com>
2023-05-08 01:41:27 -0400

    initial commit :tada:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,339 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
+import ReactLeafletKml from "react-leaflet-kml";
+import toGeoJSON from "@mapbox/togeojson";
+
+type RouteVehicle = {
+  BlockFareboxId: number;
+  CommStatus: string;
+  Destination: string;
+  Deviation: number;
+  Direction: string;
+  DirectionLong: string;
+  DisplayStatus: string;
+  StopId: number;
+  CurrentStatus: string;
+  DriverName: string;
+  GPSStatus: number;
+  Heading: number;
+  LastStop: string;
+  LastUpdated: string;
+  Latitude: number;
+  Longitude: number;
+  Name: string;
+  OccupancyStatus: number;
+  OnBoard: number;
+  OpStatus: string;
+  RouteId: number;
+  RunId: number;
+  Speed: number;
+  TripId: number;
+  VehicleId: number;
+  SeatingCapacity: number;
+  TotalCapacity: number;
+  PropertyName: string;
+  OccupancyStatusReportLabel: string;
+};
+
+type RouteStop = {
+  Description: string;
+  IsTimePoint: boolean;
+  Latitude: number;
+  Longitude: number;
+  Name: string;
+  StopId: number;
+  StopRecordId: number;
+
+  // Injected by us
+  Route: string;
+};
+
+type RouteDetails = {
+  Color: string;
+  Directions: {
+    Dir: string;
+    DirectionDesc: string;
+    DirectionIconFileName: string;
+  }[];
+  GoogleDescription: string;
+  Group: string;
+  IncludeInGoogle: boolean;
+  IsHeadway: boolean;
+  IsHeadwayMonitored: boolean;
+  IsVisible: boolean;
+  IvrDescription: string;
+  LongName: string;
+  Messages: {
+    Cause: number;
+    CauseReportLabel: string;
+    ChannelMessages: {
+      ChannelId: number;
+      ChannelMessageTranslations: {
+        LanguageId: string;
+        TranslatedText: string;
+      }[];
+      Message: string;
+    }[];
+    DaysOfWeek: number;
+    Effect: number;
+    EffectReportLabel: string;
+    FromDate: string;
+    FromTime: string;
+    Message: string;
+    MessageId: number;
+    MessageTranslations: {
+      Lang: string;
+      Text: string;
+    }[];
+    Priority: number;
+    PublicAccess: number;
+    Published: boolean;
+    Routes: number[];
+    Signs: number[];
+    ToDate: string;
+    ToTime: string;
+    URL: string;
+    Detour_Id: number;
+  }[];
+  RouteAbbreviation: string;
+  RouteId: number;
+  RouteRecordId: number;
+  RouteStops: {
+    Direction: string;
+    RouteId: number;
+    SortOrder: number;
+    StopId: number;
+  }[];
+  RouteTraceFilename: string;
+  RouteTraceHash64: string;
+  ShortName: string;
+  SortOrder: number;
+  Stops: {
+    Description: string;
+    IsTimePoint: boolean;
+    Latitude: number;
+    Longitude: number;
+    Name: string;
+    StopId: number;
+    StopRecordId: number;
+  }[];
+  TextColor: string;
+  Vehicles: {
+    BlockFareboxId: number;
+    CommStatus: string;
+    Destination: string;
+    Deviation: number;
+    Direction: string;
+    DirectionLong: string;
+    DisplayStatus: string;
+    StopId: number;
+    CurrentStatus: string;
+    DriverName: string;
+    GPSStatus: number;
+    Heading: number;
+    LastStop: string;
+    LastUpdated: string;
+    Latitude: number;
+    Longitude: number;
+    Name: string;
+    OccupancyStatus: number;
+    OnBoard: number;
+    OpStatus: string;
+    RouteId: number;
+    RunId: number;
+    Speed: number;
+    TripId: number;
+    VehicleId: number;
+    SeatingCapacity: number;
+    TotalCapacity: number;
+    PropertyName: string;
+    OccupancyStatusReportLabel: string;
+  }[];
+  DetourActiveMessageCount: number;
+};
 
 export default function Home() {
+  const [routeIds, setRouteIds] = useState([
+    "10001",
+    "10002",
+    "10003",
+    "10004",
+    "10005",
+    "10006",
+    "10007",
+    "10009",
+    "10010",
+    "10011",
+    "10012",
+    "10014",
+    "10017",
+    "10020",
+    "10021",
+    "10921",
+    "10023",
+    "10024",
+    "10029",
+    "20030",
+    "20031",
+    "20033",
+    "20034",
+    "20035",
+    "20036",
+    "20038",
+    "30039",
+    "30041",
+    "30042",
+    "30043",
+    "30943",
+    "30044",
+    "20045",
+    "20046",
+    "30048",
+    "10073",
+    "20079",
+    "10090",
+    "10092",
+  ]);
+  const [vehicles, setVehicles] = useState<RouteVehicle[]>([]);
+  const [stops, setStops] = useState<RouteStop[]>([]);
+  const [routesGeoJSON, setRoutesGeoJSON] = useState<any[]>([]);
+  const [routeDetails, setRouteDetails] = useState<RouteDetails[]>([]);
+
+  const fetchVehicles = () => {
+    fetch(
+      `https://bustracker.pvta.com/InfoPoint/rest/Vehicles/GetAllVehiclesForRoutes?routeIDs=${routeIds.join(
+        ","
+      )}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setVehicles(res);
+      });
+  };
+
+  const fetchStops = () => {
+    fetch(
+      `https://bustracker.pvta.com/InfoPoint/rest/Stops/GetAllStopsForRoutes?routeIDs=${routeIds.join(
+        ","
+      )}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setStops(res);
+      });
+  };
+
+  const fetchAllRouteDetails = () => {
+    fetch(
+      `https://bustracker.pvta.com/InfoPoint/rest/RouteDetails/GetAllRouteDetails`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setRouteDetails(res);
+      });
+  };
+
+  const fetchKML = async () => {
+    const parser = new DOMParser();
+
+    for (const route of routeDetails.filter((r) => r.Vehicles.length > 0)) {
+      await fetch(
+        `https://bustracker.pvta.com/InfoPoint/Resources/Traces/${route.RouteTraceFilename}`
+      )
+        .then((res) => res.text())
+        .then((res) => {
+          const xml = parser.parseFromString(res, "text/xml");
+
+          const geoJSON = toGeoJSON.kml(xml, { styles: true });
+
+          setRoutesGeoJSON((prev) => [
+            ...prev,
+            { json: geoJSON, color: route.Color },
+          ]);
+        });
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(fetchVehicles, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetchVehicles();
+    fetchStops();
+    fetchAllRouteDetails();
+  }, []);
+
+  useEffect(() => {
+    fetchKML();
+  }, [routeDetails]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative w-screen h-screen">
+      <div className="absolute bottom-0 z-50 flex w-full my-4">
+        <div className="flex flex-col justify-self-center items-center justify-center w-full h-full">
+          <span className="px-4 py-3 bg-gray-800 rounded-md bg-opacity-75">
+            <span className="text-xl font-bold text-white">
+              Only showing active routes
+            </span>
+          </span>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="absolute top-0 right-4 z-50 justify-end flex my-4 mx-auto">
+        <div className="flex flex-col  space-y-1 w-full h-full">
+          <span className="px-4 py-3 bg-gray-800 rounded-md bg-opacity-75">
+            <span className="text-xl font-bold text-white">
+              {vehicles.length} vehicles
+            </span>
+          </span>
+          <span className="px-4 py-3 flex flex-col bg-gray-800 rounded-md bg-opacity-75">
+            <span className="text-xl font-bold text-white">
+              It&apos;s currently:
+            </span>
+            <span className="text-lg font-semibold text-white">
+              {new Date().toLocaleTimeString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}
+            </span>
+          </span>
+        </div>
+      </div>
+      <MapContainer
+        center={[42.3055, -72.5208]}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ width: "100%", height: "100%" }}
+        id="map"
+        className="z-0 "
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=57ca0087bbbf48849ddc3d4ede2fbe10"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {routesGeoJSON.length > 0 &&
+          routesGeoJSON.map((r, i) => (
+            <GeoJSON
+              key={i}
+              data={r.json}
+              pathOptions={{ color: `#${r.color}`, opacity: 0.75 }}
+            />
+          ))}
+        {vehicles.map((vehicle) => (
+          <Marker
+            key={vehicle.VehicleId}
+            position={[vehicle.Latitude, vehicle.Longitude]}
+          >
+            <Popup>Route 38</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </main>
-  )
+  );
 }
